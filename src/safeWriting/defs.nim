86d0052f36c61a8073b06716ab4eeeb772105d12
Thiago Navarro <thiago@oxyoy.com>
2021-07-08 14:48:47 -0300

    Added better input filter

--- src/safeWriting/defs.nim
+++ src/safeWriting/defs.nim
@@ -1,18 +1,37 @@
 #[
   Created at: 07/08/2021 13:03:35 Thursday
-  Modified at: 07/08/2021 01:50:50 PM Thursday
+  Modified at: 07/08/2021 02:48:16 PM Thursday
 ]#
 
 ##[
   defs
 ]##
 
+from std/strutils import join
+
 const inputElements* = [
-  "input:not([type=date]):not([type=datetime]):not([type=month]):not([type=button]):not([type=submit])",
+  "input" & [
+    ":not([type=button])",
+    ":not([type=checkbox])",
+    ":not([type=color])",
+    ":not([type=date])",
+    ":not([type=datetime])",
+    ":not([type=file])",
+    ":not([type=hidden])",
+    ":not([type=image])",
+    ":not([type=month])",
+    ":not([type=radio])",
+    ":not([type=range])",
+    ":not([type=reset])",
+    ":not([type=submit])",
+    ":not([type=time])",
+    ":not([type=week])",
+  ].join(""),
   "textarea",
   "[contenteditable=true]"
 ]
-
+static:
+  echo inputElements
 const inputTriggerEvents* = [
   "focus",
   "click",
